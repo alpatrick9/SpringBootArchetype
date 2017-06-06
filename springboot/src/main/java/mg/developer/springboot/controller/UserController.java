2d63a72fcfd5858bbab33ed0dd2ed6229f85736e
Patrick <patrick@threeshells.ovh>
2017-06-06 14:54:36 +0300

    tweak organize import

--- springboot/src/main/java/mg/developer/springboot/controller/UserController.java
+++ springboot/src/main/java/mg/developer/springboot/controller/UserController.java
@@ -2,7 +2,6 @@ package mg.developer.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
